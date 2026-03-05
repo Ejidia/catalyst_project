3fc4ada61c9b8de298dd8c78e9e2f30258f48aea
Ejidia Uwabeza <uwabezaejidia94@gmail.com>
2026-03-05 12:39:25 +0300

    Add password reset URLs to main urls.py

--- kgl/urls.py
+++ kgl/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path
 from karibu import views
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -55,6 +56,12 @@ urlpatterns = [
     path('salesagent_dashboard/', views.salesagent_dashboard, name='salesagent_dashboard'),
 
     path('receipt/final/<int:receipt_id>/', views.final_receipt, name='final_receipt'),
+
+    # Password reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='ebook/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='ebook/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='ebook/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='ebook/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
 
